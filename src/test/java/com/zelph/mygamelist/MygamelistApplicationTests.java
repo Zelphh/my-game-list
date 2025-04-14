3e312c8915a00bdabf38bf3f60d26aa69260e8a5
Matheus Melle Tártari <matheusmelle2006@gmail.com>
2025-04-14 19:27:15 -0300

    Projeto criado

---
+++ src/test/java/com/zelph/mygamelist/MygamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelph.mygamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
